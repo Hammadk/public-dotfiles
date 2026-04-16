2e9be6f955d90609baa70fac2e6b182b750c35bc
Hammad Khalid <hammad.khalid@shopify.com>
2026-04-16 13:57:20 -0400

    Move ghostty config to dotfiles too

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,24 +7,44 @@ symlink() {
   ln -nsf $1 $2
 }
 
-for file in home/.[^.]*; do
-  path="$(pwd)/$file"
-  base=$(basename $file)
-  target="$HOME/$(basename $file)"
+link_target() {
+  local path=$1
+  local target=$2
+  local label=$3
 
   if [[ -h $target && ($(readlink $target) == $path)]]; then
-    echo -e "\x1B[90m~/$base is symlinked to your dotfiles.\x1B[39m"
+    echo -e "\x1B[90m$label is symlinked to your dotfiles.\x1B[39m"
   elif [[ -f $target && $(md5 $path) == $(md5 $target) ]]; then
-    echo -e "\x1B[32m~/$base exists and was identical to your dotfile.  Overriding with symlink.\x1B[39m"
+    echo -e "\x1B[32m$label exists and was identical to your dotfile.  Overriding with symlink.\x1B[39m"
     symlink $path $target
   elif [[ -a $target ]]; then
-    read -p "\x1B[33m~/$base exists and differs from your dotfile. Override?  [yn]\x1B[39m" -n 1
+    read -p "\x1B[33m$label exists and differs from your dotfile. Override?  [yn]\x1B[39m" -n 1
 
     if [[ $REPLY =~ [yY]* ]]; then
       symlink $path $target
     fi
   else
-    echo -e "\x1B[32m~/$base does not exist. Symlinking to dotfile.\x1B[39m"
+    echo -e "\x1B[32m$label does not exist. Symlinking to dotfile.\x1B[39m"
     symlink $path $target
   fi
+}
+
+# Symlink dotfiles from home/ to ~/
+for file in home/.[^.]*; do
+  [[ $(basename $file) == ".config" ]] && continue
+  path="$(pwd)/$file"
+  base=$(basename $file)
+  link_target $path "$HOME/$base" "~/$base"
 done
+
+# Symlink ~/.config/* subdirectories individually (don't clobber ~/.config)
+if [[ -d home/.config ]]; then
+  mkdir -p "$HOME/.config"
+  for dir in home/.config/*/; do
+    [[ -d $dir ]] || continue
+    path="$(pwd)/$dir"
+    path="${path%/}"  # strip trailing slash
+    base=$(basename $dir)
+    link_target "$path" "$HOME/.config/$base" "~/.config/$base"
+  done
+fi
